42237e2f15d7c0404ca9e7223bfc65f5577231f9
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-03-26 13:52:45 +0200

    Reactivities

--- obj/Debug/net8.0/Portofoliu.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portofoliu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portofoliu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697838d23f46ebf3a8eefbcf3a87e16a7d424a96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8bf270700f2cbc03a4781923e43e15c19c3afae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portofoliu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portofoliu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
